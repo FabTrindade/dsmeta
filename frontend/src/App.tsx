907cf064117b79ad7e15e2d32943a1f89c0f6a5a
FabTrindade <fabrtrindade@gmail.com>
2022-07-17 17:39:49 -0300

    First comonent.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import React from 'react';
+import NotificationButton from './NotificationButton';
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Hello world!</h1>
+      <NotificationButton />
+    </>
+
+
   )
 }
 
