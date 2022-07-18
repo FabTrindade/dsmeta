3e7496c444e8b7b05b8ebbdae30ac9cbf537c730
FabTrindade <fabrtrindade@gmail.com>
2022-07-17 23:12:57 -0300

    Send first test sms.

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dsmeta.controllers;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.sevices.SaleService;
+import com.devsuperior.dsmeta.sevices.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
-	
+
+		
 	@Autowired
 	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;
 	
 	@GetMapping
 	public Page<Sale> findSales(
@@ -24,6 +33,11 @@ public class SaleController {
 			@RequestParam(value="maxDate", defaultValue = "") String maxDate, 
 			Pageable pageable){
 		return service.findSales(minDate, maxDate, pageable);
+	}
+	
+	@GetMapping ("/notification")
+	public void notifySms() {
+		smsService.sendSms();
 	}
 
 }
